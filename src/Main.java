ddff8a124a8c684e1382e39a4ba360e84b8c0306
Harsh Kumar <harsh19043@iiitd.ac.in>
2020-11-15 13:52:07 +0530

    Added Basic Obstacle Example Refactor: folder 'screens' changed to 'scenes'
    Signed-off-by: Harsh Kumar <harsh19043@iiitd.ac.in>

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import javafx.animation.Interpolator;
+import javafx.animation.RotateTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import org.jetbrains.annotations.NotNull;
 
 public class Main extends Application {
@@ -13,9 +17,18 @@ public class Main extends Application {
 
     @Override
     public void start(@NotNull Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("screens/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("scenes/sample.fxml"));
         // Adding stylesheet to the root
         root.getStylesheets().add(getClass().getResource("style/style.css").toExternalForm());
+
+        RotateTransition rotateTransition = new RotateTransition();
+        rotateTransition.setAxis(Rotate.Z_AXIS);
+        rotateTransition.setByAngle(360);
+        rotateTransition.setCycleCount(1000);
+        rotateTransition.setDuration(Duration.millis(2000));
+        rotateTransition.setInterpolator(Interpolator.LINEAR);
+        rotateTransition.setNode(root);
+        rotateTransition.play();
 
         primaryStage.setTitle("Color Switch");
         Scene scene = new Scene(root, 500, 500);
