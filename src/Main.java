5bef66b4466e400f8b95bc18ee79154ff6b74033
Harsh Kumar <harsh19043@iiitd.ac.in>
2020-11-22 13:31:45 +0530

    Bug: Adjust stage size according to Screen Resolution (#14)
    Signed-off-by: Harsh Kumar <harsh19043@iiitd.ac.in>

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,54 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.text.Font;
+import javafx.scene.paint.Paint;
+import javafx.scene.transform.Scale;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    static double width, height;
 
     public static void main(String[] args) {
         launch(args);
     }
 
+    private static void scale(Parent root) {
+        Scale scale = new Scale();
+        scale.setPivotY(0);;
+        scale.setPivotX(0);
+        scale.setY(height/1024);;
+        scale.setX(width/768);
+        System.out.println(root);
+        root.getTransforms().addAll(scale);
+    }
+
     protected static Scene loadSettings() throws Exception {
         Parent root = FXMLLoader.load(Main.class.getResource("scenes/settings.fxml"));
-        return new Scene(root);
+        scale(root);
+        Scene scene = new Scene(root);
+        scene.setFill(Paint.valueOf("#292929"));
+        return scene;
     }
 
     protected static Scene loadProducedBy() throws Exception {
         Parent root = FXMLLoader.load(Main.class.getResource("scenes/produced.fxml"));
-        return new Scene(root);
+        scale(root);
+        Scene scene = new Scene(root);
+        scene.setFill(Paint.valueOf("#292929"));
+        return scene;
     }
 
     protected static Scene loadAboutGame() throws Exception{
         Parent root = FXMLLoader.load(Main.class.getResource("scenes/about_game.fxml"));
-        return new Scene(root);
+        scale(root);
+        Scene scene = new Scene(root);
+        scene.setFill(Paint.valueOf("#292929"));
+        return scene;
     }
 
     protected static Scene loadStats() throws Exception{
@@ -34,7 +58,10 @@ public class Main extends Application {
 
     protected static Scene getHome() throws Exception {
         Parent root = FXMLLoader.load(Main.class.getResource("scenes/home.fxml"));
-        return new Scene(root);
+        scale(root);
+        Scene scene = new Scene(root);
+        scene.setFill(Paint.valueOf("#292929"));
+        return scene;
     }
 
     @Override
@@ -42,12 +69,16 @@ public class Main extends Application {
         primaryStage.getIcons().add(new Image("/assets/color-switch-icon.png"));
         primaryStage.setTitle("Color Switch");
 
+        Rectangle2D viewPort = Screen.getPrimary().getVisualBounds();
+        height = viewPort.getHeight();
+        width = 3*height/4;
+
         Scene scene = getHome();
 //        Scene scene = loadStats();
 
         primaryStage.setScene(scene);
-        primaryStage.setHeight(680);
-        primaryStage.setWidth(600);
+        primaryStage.setHeight(height);
+        primaryStage.setWidth(width);
         primaryStage.show();
     }
 
