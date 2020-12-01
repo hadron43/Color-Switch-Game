53336b868665fcffde77aca514c018cedf696c76
Varun Khurana <varun19124@iiitd.ac.in>
2020-12-01 17:48:51 +0530

    Modified moveUp and freeFall transitions to Ball Key pressed functionality to be added

--- src/elements/controllers/BallController.java
+++ src/elements/controllers/BallController.java
@@ -1,6 +1,7 @@
 package elements.controllers;
 
 import javafx.animation.Animation;
+import javafx.animation.Transition;
 import javafx.animation.TranslateTransition;
 import javafx.event.Event;
 import javafx.event.EventHandler;
@@ -15,6 +16,8 @@ import javafx.scene.shape.Circle;
 import javafx.util.Duration;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class BallController implements Initializable {
@@ -23,33 +26,47 @@ public class BallController implements Initializable {
     public Pane ball;
     @FXML
     public Circle circle_ball;
+    private final List<Transition> ball_transitions;
 
-    public BallController() { }
+    public BallController() {
+        this.ball_transitions = new ArrayList<Transition>();
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        // Check how to stop this transition on Mouse Click
 
-//        TranslateTransition t_trans = new TranslateTransition();
-//        t_trans.setByY(-100);
-//        t_trans.setDuration(Duration.millis(750));
-//        t_trans.setCycleCount(Animation.INDEFINITE);
-//        t_trans.setAutoReverse(true);
-//        t_trans.setNode(ball);
-//        t_trans.play();
+        // Welcome translate transition
+        TranslateTransition t_trans = new TranslateTransition();
+        t_trans.setByY(-100);
+        t_trans.setDuration(Duration.millis(750));
+        t_trans.setCycleCount(Animation.INDEFINITE);
+        t_trans.setAutoReverse(true);
+        t_trans.setNode(ball);
+        t_trans.play();
+        ball_transitions.add(t_trans);
+
+        // move up transition
+        TranslateTransition move_up = new TranslateTransition(Duration.millis(250), ball);
+        move_up.setByY(-100);
+        ball_transitions.add(move_up);
+
+        // free fall transition
+        TranslateTransition free_fall = new TranslateTransition(Duration.millis(250), ball);
+        free_fall.setByY(75);
+        ball_transitions.add(free_fall);
+
+
     }
 
     public void moveUp(MouseEvent mouseEvent) {
-        TranslateTransition move_up = new TranslateTransition(Duration.millis(250), ball);
-        move_up.setByY(-100);
-        move_up.play();
+        ball_transitions.get(0).stop();
+        ball_transitions.get(1).play();
     }
 
     public void freeFall(MouseEvent me) {
-        TranslateTransition free_fall = new TranslateTransition(Duration.millis(250), ball);
-        free_fall.setByY(75);
-        free_fall.play();
+        ball_transitions.get(2).play();
     }
+
 
     // Key functionality to be added:
 
