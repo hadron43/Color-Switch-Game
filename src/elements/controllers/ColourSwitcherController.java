474cf84bc3ba2935d65f242e3dc08f31c5369900
Varun Khurana <varun19124@iiitd.ac.in>
2020-12-02 20:18:04 +0530

    Added ColourSwitcher Controller

--- src/elements/controllers/ColourSwitcherController.java
+++ src/elements/controllers/ColourSwitcherController.java
@@ -1,4 +1,27 @@
 package elements.controllers;
 
-public class ColourSwitcherController {
+import javafx.animation.*;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.Pane;
+import javafx.scene.transform.Rotate;
+import javafx.util.Duration;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ColourSwitcherController implements Initializable {
+    @FXML private Pane colourSwitcher;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        RotateTransition rotateCS = new RotateTransition();
+        rotateCS.setAxis((Rotate.Z_AXIS));
+        rotateCS.setByAngle(360);
+        rotateCS.setCycleCount(Animation.INDEFINITE);
+        rotateCS.setInterpolator(Interpolator.LINEAR);
+        rotateCS.setDuration(Duration.millis(3000));
+        rotateCS.setNode(colourSwitcher);
+    }
 }
